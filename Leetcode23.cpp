3bdd79ded406b5316bf96205d69b4fd32854551e
Kanchan-ai05 <shawkanchan580@gmail.com>
2026-09-22 23:43:46 +0530

    Add leetcode 233

---
+++ Leetcode23.cpp
@@ -0,0 +1,38 @@
+
+
+class Solution {
+public:
+
+    struct Compare {
+        bool operator()(ListNode* a, ListNode* b) {
+            return a->val > b->val;
+        }
+    };
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+
+        priority_queue<ListNode*, vector<ListNode*>, Compare> pq;
+
+        for (ListNode* node : lists) {
+            if (node)
+                pq.push(node);
+        }
+
+        ListNode dummy(0);
+        ListNode* tail = &dummy;
+
+        while (!pq.empty()) {
+
+            ListNode* smallest = pq.top();
+            pq.pop();
+
+            tail->next = smallest;
+            tail = tail->next;
+
+            if (smallest->next)
+                pq.push(smallest->next);
+        }
+
+        return dummy.next;
+    }
+};
